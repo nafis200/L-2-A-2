ce266f0c374ab9de9ec15a2be6e7a06499550bef
nafis200 <nafisahamed14@gmail.com>
2025-01-31 20:50:06 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 
 
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173','https://carshopfronted.vercel.app'], credentials: true }));
 
 app.use(cookieParser());
 
